31b00a56c22eaac481fb3297aa626598c7f5e3c7
zarina <zarinakamalova117@gmail.com>
2023-03-08 12:02:44 +0600

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a, b;
+        a=13;
+        b=19;
+        float y;
+        y=(9*a^2-5*b+2+a-7)*(a+b-4*a*b)/2;
+        System.out.println("result:"+ y);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char symbol1= 'c';
+        char symbol2= 'h';
+        char symbol3= 'a';
+        char symbol4= 'r';
+
+        System.out.println((int)symbol1);
+        System.out.println((int)symbol2);
+        System.out.println((int)symbol3);
+        System.out.println((int)symbol4);
     }
 }
